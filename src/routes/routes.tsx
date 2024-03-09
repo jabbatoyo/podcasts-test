88ac0c9fc42f5c15f73558864ad75e1265501b4e
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-09 21:01:52 +0100

    add route views

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,26 +1,33 @@
 import { RouteObject } from "react-router-dom";
 import { loaderPodcast, loaderPodcasts } from "./loaders";
 
+//components
+import { Layout } from "../components";
+// views
+import HomePage from "../pages/HomePage";
+import DetailPage from "../pages/DetailPage";
+import EpisodePage from "../pages/EpisodePage";
+
 export const BASE_URL = "/";
 
 const routes: RouteObject[] = [
   {
     path: BASE_URL,
-    element: <h1>Layout</h1>,
+    element: <Layout />,
     children: [
       {
         path: BASE_URL,
-        element: <h1>HOME</h1>,
+        element: <HomePage />,
         loader: loaderPodcasts,
       },
       {
         path: `${BASE_URL}podcast/{podcastId}`,
-        element: <h1>DETAIL</h1>,
+        element: <DetailPage />,
         loader: () => loaderPodcast,
       },
       {
         path: `${BASE_URL}podcast/{podcastId}/episode/{episodeId}`,
-        element: <h1>Episidio</h1>,
+        element: <EpisodePage />,
       },
     ],
   },
