db3e258fe763696b7d02e3e8a20aec80978043a1
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-12 13:23:39 +0100

    modify loader to get data in localStorage

--- src/routes/loaders.ts
+++ src/routes/loaders.ts
@@ -1,8 +1,23 @@
 import { LoaderFunction } from "react-router-dom";
-import { getPodcasts, getPodcast } from "../services";
+import { getPodcasts, getEpidodes, PodcastDto } from "../services";
+import { APP_LOCALSTORAGE_KEY } from "../config/constants";
+import { isLocalStogageExpired } from "../services/utils";
 
 export const loaderPodcasts: LoaderFunction = async () => {
+  const podcastsLocalStorage = JSON.parse(
+    localStorage.getItem(APP_LOCALSTORAGE_KEY) as any
+  );
+
+  if (
+    podcastsLocalStorage ||
+    !isLocalStogageExpired(podcastsLocalStorage?.timestamp)
+  ) {
+    const { value } = podcastsLocalStorage;
+    return value;
+  }
   const res = await getPodcasts();
+  const localObject = { value: res, timestamp: new Date().getTime() };
+  localStorage.setItem(APP_LOCALSTORAGE_KEY, JSON.stringify(localObject));
   return res;
 };
 
