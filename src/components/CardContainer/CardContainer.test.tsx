da1b00fe6fd119be6107c9c38700af2f234f6746
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-12 14:18:12 +0100

    add cardContainer test

--- src/components/CardContainer/CardContainer.test.tsx
+++ src/components/CardContainer/CardContainer.test.tsx
@@ -1,35 +1,47 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 
 //component
 import CardContainer from "./index";
-import { FavouriteProvider } from "../../context/favouritesContext";
-import routes from "../../routes/routes";
 
 describe("CardContainer test", () => {
+  const podcasts = [
+    {
+      id: 1,
+      author: "author 1",
+      title: "title podcast 1",
+      description: " test descripcion podcast",
+      img: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts116/v4/23/af/d5/23afd583-d4a1-a5a5-024f-9a8556aa4aa1/mza_10050319290971063684.jpg/170x170bb.png",
+    },
+    {
+      id: 2,
+      author: "author 2",
+      title: "title podcast 2",
+      description: " test descripcion podcast",
+      img: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts116/v4/23/af/d5/23afd583-d4a1-a5a5-024f-9a8556aa4aa1/mza_10050319290971063684.jpg/170x170bb.png",
+    },
+    {
+      id: 3,
+      author: "author 3",
+      title: "title podcast 3",
+      description: " test descripcion podcast",
+      img: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts116/v4/23/af/d5/23afd583-d4a1-a5a5-024f-9a8556aa4aa1/mza_10050319290971063684.jpg/170x170bb.png",
+    },
+  ];
+
   beforeEach(() => {
-    render(<CardContainer />, {
-      wrapper: () => (
-        <FavouriteProvider>
-          <RouterProvider
-            router={createMemoryRouter(routes, {
-              initialEntries: ["/"],
-            })}
-          />
-        </FavouriteProvider>
-      ),
-    });
+    render(
+      <BrowserRouter>
+        <CardContainer podcasts={podcasts} />
+      </BrowserRouter>
+    );
   });
 
-  test("Render component card container", async () => {
-    await waitFor(() => {
-      expect(screen.getAllByTestId("card-container")).toBeTruthy();
-    });
+  test("Render component card container", () => {
+    expect(screen.getByTestId("card-container")).toBeTruthy();
   });
 
-  test("Render component card container render all element 3 card", async () => {
-    await waitFor(() => {
-      expect(screen.getAllByTestId("card-character-content")).toHaveLength(3);
-    });
+  test("Render component card container render all element 3 card", () => {
+    expect(screen.getAllByTestId("card-podcast-content")).toHaveLength(3);
   });
 });
