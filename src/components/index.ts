7d79842a4ca5171d67a87378e810433dfceb6e8b
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-12 14:18:43 +0100

    remove unused export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,3 @@ export { default as Header } from "./Header";
 export { default as CardContainer } from "./CardContainer";
 export { default as Card } from "./Card";
 export { default as Loading } from "./icons/Loading";
-export { default as DetailCardPodcast } from "../pages/DetailPage/components/DetailCardPodcast";
