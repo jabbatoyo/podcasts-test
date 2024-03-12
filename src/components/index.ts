27ae87edbf0a49ef016e93b0fb5c99a8a7758a1f
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-12 13:25:33 +0100

    add loading icon component

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Layout } from "./Layout";
+export { default as Header } from "./Header";
+export { default as CardContainer } from "./CardContainer";
+export { default as Card } from "./Card";
+export { default as Loading } from "./icons/Loading";
+export { default as DetailCardPodcast } from "../pages/DetailPage/components/DetailCardPodcast";
