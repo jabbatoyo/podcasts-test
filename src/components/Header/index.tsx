99c51247b5280d30a9eea7a059c183da4fef5455
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-12 14:18:27 +0100

    add header component test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,13 @@ import { Container } from "./Header.styles";
 
 const Header = ({ state }: { state: string }) => {
   return (
-    <Container>
+    <Container data-testid="header-container">
       <div className="header-content">
         <Link className="title-logo" to="/">
           Podcaster
         </Link>
         {state === "loading" && (
-          <div className="loading-content">
+          <div className="loading-content" data-testid="loading-content">
             <Loading />
           </div>
         )}
